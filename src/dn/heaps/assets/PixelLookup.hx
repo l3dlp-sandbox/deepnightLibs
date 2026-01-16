417a34581846cebfb9edc81101bf7e85fcdb7e26
Sebastien Benard <github@deepnight.net>
2026-01-16 10:09:11 +0100

    ADD replaceColorsPixels

--- src/dn/heaps/assets/PixelLookup.hx
+++ src/dn/heaps/assets/PixelLookup.hx
@@ -1,7 +1,11 @@
 package dn.heaps.assets;
 
+import dn.heaps.slib.SpriteLib;
+import hxd.Pixels;
+import dn.Col;
+
 class PixelLookup extends dn.Process {
-	public static function fromSlib(slib:dn.heaps.slib.SpriteLib, ?slibPixels:hxd.Pixels, lookupColor:dn.Col) : Map<Int,PixelPoint> {
+	public static function fromSlib(slib:SpriteLib, ?slibPixels:Pixels, lookupColor:Col) : Map<Int,PixelPoint> {
 		var pixels = slibPixels ?? slib.tile.getTexture().capturePixels();
 
 		var col = lookupColor.withAlphaIfMissing();
@@ -17,7 +21,7 @@ class PixelLookup extends dn.Process {
 	}
 
 
-	public static function replaceColor(slib:dn.heaps.slib.SpriteLib, lookupColor:dn.Col, newColorWithAlpha:dn.Col, ignoreSourceAlpha:Bool) {
+	public static function replaceColorLib(slib:SpriteLib, lookupColor:Col, newColorWithAlpha:Col, ignoreSourceAlpha:Bool) {
 		var pixels = slib.tile.getTexture().capturePixels();
 
 		var c : Col = 0;
@@ -39,9 +43,24 @@ class PixelLookup extends dn.Process {
 	 * @param ignoreSourceAlpha If true, the alpha channel of the source pixel is ignored when matching (only RGB is matched).
 	 *                          Otherwise, the alpha channel is considered when matching colors.
 	 */
-	public static function replaceColors(slib:dn.heaps.slib.SpriteLib, remap:Map<dn.Col, dn.Col>, ignoreSourceAlpha:Bool) {
+	public static function replaceColorsLib(slib:SpriteLib, remap:Map<Col, Col>, ignoreSourceAlpha:Bool) {
 		var pixels = slib.tile.getTexture().capturePixels();
+		replaceColorsPixels(pixels, remap, ignoreSourceAlpha);
 
+		var newTile = h2d.Tile.fromPixels(pixels);
+		slib.tile.switchTexture(newTile);
+	}
+
+
+	/**
+	 * Replace multiple colors at once.
+	 *
+	 * @param pixels The pixels to modify (WARNING: this modifies the pixels in place).
+	 * @param remap A map from source color to target color.
+	 * @param ignoreSourceAlpha If true, the alpha channel of the source pixel is ignored when matching (only RGB is matched).
+	 *                          Otherwise, the alpha channel is considered when matching colors.
+	 */
+	public static function replaceColorsPixels(pixels:Pixels, remap:Map<Col, Col>, ignoreSourceAlpha:Bool) {
 		var c : Col = 0;
 		for(x in 0...pixels.width)
 		for(y in 0...pixels.height) {
@@ -51,12 +70,10 @@ class PixelLookup extends dn.Process {
 				pixels.setPixel( x,y, newCol );
 			}
 		}
-		var newTile = h2d.Tile.fromPixels(pixels);
-		slib.tile.switchTexture(newTile);
 	}
 
 
-	static function lookupSubPixels(pixels:hxd.Pixels, x:Int, y:Int, w:Int, h:Int, col:Col) : Null<PixelPoint> {
+	static function lookupSubPixels(pixels:Pixels, x:Int, y:Int, w:Int, h:Int, col:Col) : Null<PixelPoint> {
 		for(px in x...x+w)
 		for(py in y...y+h)
 			if( pixels.getPixel(px,py)==col )
